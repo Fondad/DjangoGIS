02c3ecac206171cad7a96e1982933f27e44fdfd2
Vsevolod Demidov <fondad@ukr.net>
2017-03-07 11:44:24 +0200

    URLS and Home Template were created. Home views was added.

--- gis/urls.py
+++ gis/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+
+import gis.views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('gis.urls')),
+    url(r'^$', gis.views.home, name='home'),
 ]
