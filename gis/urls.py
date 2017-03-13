507a6d0efa6d9c4db0c7895152954d1871dafaf4
Vsevolod Demidov <fondad@ukr.net>
2017-03-13 11:45:29 +0200

    Articles were added in Home views. About urls, views, template were created

--- gis/urls.py
+++ gis/urls.py
@@ -19,4 +19,5 @@ import gis.views
 
 urlpatterns = [
     url(r'^$', gis.views.home, name='home'),
+    url(r'^about/$', gis.views.about, name='about'),
 ]
--- gis/views.py
+++ gis/views.py
@@ -3,6 +3,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from gis.models import Articles
+
+
 def home(request):
     # return HttpResponse("Hello world")
-    return render(request, 'gis/home.html')
+    # return render(request, 'gis/home.html')
+    articles = Articles.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'gis/home.html', context)
+
+
+def about(request):
+    return render(request, 'gis/about.html')
